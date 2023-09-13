cbea7a99612418d7c0eae7e27bdd851d20a8d6d7
Fetivol <Fetisowova@gmail.com>
2023-09-13 18:31:37 +0200

    Refactoring for hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FbManagement } from './FbManagement/FbManagement';
 import { Statistics } from './Statistics/Statistics';
 import { Wrapper } from './Wrapper/Wrapper';
@@ -6,53 +6,104 @@ import { Notification } from './Notification/Notification';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [options, setOptions] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const { good, neutral, bad } = options;
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  addFeedback = option => {
-    this.setState(pState => ({
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  const addFeedback = option => {
+    setOptions(pState => ({
+      ...pState,
       [option]: (pState[option] += 1),
     }));
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+  return (
+    <Layout>
+      <Wrapper title="Please leave feedback">
+        <FbManagement
+          options={Object.keys(options)}
+          onClick={addFeedback}
+        ></FbManagement>
+      </Wrapper>
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
+      <Wrapper title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Wrapper>
+      <GlobalStyle></GlobalStyle>
+    </Layout>
+  );
+};
 
-  render() {
-    return (
-      <Layout>
-        <Wrapper title="Please leave feedback">
-          <FbManagement
-            options={Object.keys(this.state)}
-            onClick={this.addFeedback}
-          ></FbManagement>
-        </Wrapper>
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
-        <Wrapper title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Wrapper>
-        <GlobalStyle></GlobalStyle>
-      </Layout>
-    );
-  }
-}
+//   addFeedback = option => {
+//     this.setState(pState => ({
+//       [option]: (pState[option] += 1),
+//     }));
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   };
+
+//   render() {
+//     return (
+//       <Layout>
+//         <Wrapper title="Please leave feedback">
+//           <FbManagement
+//             options={Object.keys(this.state)}
+//             onClick={this.addFeedback}
+//           ></FbManagement>
+//         </Wrapper>
+
+//         <Wrapper title="Statistics">
+//           {this.countTotalFeedback() ? (
+//             <Statistics
+//               good={this.state.good}
+//               neutral={this.state.neutral}
+//               bad={this.state.bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             ></Statistics>
+//           ) : (
+//             <Notification message="There is no feedback"></Notification>
+//           )}
+//         </Wrapper>
+//         <GlobalStyle></GlobalStyle>
+//       </Layout>
+//     );
+//   }
+// }
